011fcd549858a45169fe60ce7b6cc59e6b2c014d
Ghost Liu <deluxghost@users.noreply.github.com>
2015-01-22 17:24:32 +0800

    Update mcserv.sh

--- mcserv.sh
+++ mcserv.sh
@@ -1,22 +1,32 @@
 #!/bin/bash
 M_OLDPWD=$OLDPWD
 M_PWD=$PWD
-DIR='<YOUR MINECRAFT DIR HERE, NO "/">'
-LOGDIR=$DIR/srvlog/launcher.log
+# Modify the 3 lines first
+DIR=$HOME/mc # Minecraft Directory
+LOGDIR=$DIR/srvlog # Log Directory
+LOG=$LOGDIR/launcher.log # Log File
+[[ -d "$LOGDIR" ]] || ( mkdir "$LOGDIR" ; echo "$(date)[Info]Created log directory.">>$LOG )
 cd "$DIR"
-echo "$(date)[Info]Launching.">>$LOGDIR
+echo "$(date)[Info]Launching.">>$LOG
 MCSTAT=0
 false
-while [[ "$?" != "0" ]]; do
+while [[ "$?" != "0" || -f "$DIR/restart.lck" ]]; do
     if [[ "$MCSTAT" == "0" ]]; then
-        echo "$(date)[Info]Start Server.">>$LOGDIR
+        echo "$(date)[Info]Start server.">>$LOG
         MCSTAT=1
+    elif [[ -f "$DIR/restart.lck" ]]; then
+        echo "$(date)[Info]Restart server.">>$LOG
+        sleep 2
     else
-        echo "$(date)[Warn]Crashed. Restart Server.">>$LOGDIR
+        echo "$(date)[Warn]Crashed. Restart server.">>$LOG
         sleep 10
     fi
-    java -Xmx880M -Xms32M -server -Djava.awt.headless=true -XX:PermSize=255m -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:+UseStringCache -jar -Dfml.queryResult=confirm "$DIR/cauldron.jar"
+    if [[ -f "$DIR/restart.lck" ]]; then
+        rm "$DIR/restart.lck"
+    fi
+    # Commands here will execute before launch
+    java -server -Xmx1024M -Djava.awt.headless=true -jar $DIR/craftbukkit.jar
 done
-echo "$(date)[Info]Exit.">>$LOGDIR
+echo "$(date)[Info]Exit.">>$LOG
 cd "$M_PWD"
 OLDPWD=$M_OLDPWD
